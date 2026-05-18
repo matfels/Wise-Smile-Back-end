729a1f6854ae79ed48ceeab57fed365ec6f55633
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-05-17 21:54:26 -0300

    feat: inicializa o projeto Java como um projeto Maven
    Adiciona o arquivo de configuração pom.xml à raiz do projeto.
    
    Define a estrutura padrão de pastas do Maven (src/main/java, src/main/resources).
    
    Configura as propriedades iniciais do projeto (versão do Java, GroupId e ArtifactId).

---
+++ clinica/src/test/java/com/wise/smile/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wise.smile.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

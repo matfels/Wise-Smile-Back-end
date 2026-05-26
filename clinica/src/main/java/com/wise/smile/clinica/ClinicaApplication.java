bd7a5cdba29c90d1d44d0c9e2b796a0bd4dee891
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-05-25 22:34:38 -0300

    Realifeat: valida conexão com banco de dados e planeja estrutura do CRUD
    Realiza testes de integração bem-sucedidos entre as entidades JPA e o banco de dados.
    
    Valida as operações básicas de persistência (Save/Find) nos Repositories.
    Habilita a visualização detalhada das operações do banco de dados no console.

--- clinica/src/main/java/com/wise/smile/clinica/ClinicaApplication.java
+++ clinica/src/main/java/com/wise/smile/clinica/ClinicaApplication.java
@@ -1,13 +1,34 @@
 package com.wise.smile.clinica;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.wise.smile.clinica.entity.Usuario;
+import com.wise.smile.clinica.repositories.UsuarioRepositories;
+
 @SpringBootApplication
-public class ClinicaApplication {
+public class ClinicaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private UsuarioRepositories usuarioRepositories;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	//	Usuario usuario1 = new Usuario("Matheus Melo2", "10568319887","matheuw@matheus.com.br", "1234", "AGENDADA", true, null, null);		
+	//	usuarioRepositories.save(usuario1);
+		//lisita de usuarios
+		List<Usuario> listaDeUsuario = usuarioRepositories.findAll();
+		listaDeUsuario.forEach(usuario -> System.out.println(usuario));
+	
 	}
 
 }
--- clinica/src/main/java/com/wise/smile/clinica/entity/Usuario.java
+++ clinica/src/main/java/com/wise/smile/clinica/entity/Usuario.java
@@ -7,13 +7,35 @@ import java.time.LocalDateTime;
 @Entity // Avisa o java que essa classe não e apenas um código e sim que representa uma tabela real na base de dados.
 @Table(name = "usuarios")
 @Data
+
+
+
 public class Usuario {
+
+	// Criei um construtor vazio para poder trazer os dados para a listano (ClinicaAplicattion).
+	public Usuario() {
+	}
+	
+	
+	public Usuario(String nome, String cpf, String email, String senha, String perfil, Boolean ativo,
+			LocalDateTime dataCriacao, LocalDateTime ultimoLogin) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		this.perfil = perfil;
+		this.ativo = ativo;
+		this.dataCriacao = dataCriacao;
+		this.ultimoLogin = ultimoLogin;
+	}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, length = 100)
+
+	@Column(nullable = false, length = 100)
     private String nome;
 
     @Column(nullable = false, unique = true, length = 14)
@@ -37,7 +59,15 @@ public class Usuario {
     @Column(name = "ultimo_login")
     private LocalDateTime ultimoLogin;
 
-    // @PrePersist para preencher a data de criação automaticamente antes de salvar
+    @Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", email=" + email + ", senha=" + senha
+				+ ", perfil=" + perfil + ", ativo=" + ativo + ", dataCriacao=" + dataCriacao + ", ultimoLogin="
+				+ ultimoLogin + "]";
+	}
+
+
+	// @PrePersist para preencher a data de criação automaticamente antes de salvar
     @PrePersist
     protected void onCreate() {
         this.dataCriacao = LocalDateTime.now();

06bc57c1e6d6391080f4f58145b1d29e2b4ae774
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-05-30 18:16:45 -0300

    feat: implementa UsuarioService com regras de negócio e criptografia
    Cria a classe UsuarioService no pacote services.
    Implementa validações para garantir a unicidade de e-mail e CPF.
    
    Integra a lógica de criptografia de senhas (BCrypt) antes da persistência.

--- clinica/src/main/java/com/wise/smile/clinica/service/UsuarioService.java
+++ clinica/src/main/java/com/wise/smile/clinica/service/UsuarioService.java
@@ -1,5 +1,45 @@
 package com.wise.smile.clinica.service;
 
+import com.wise.smile.clinica.entity.Usuario;
+import com.wise.smile.clinica.repositories.UsuarioRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
 
+    @Autowired
+    private UsuarioRepositories usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Usuario registarUsuario(Usuario usuario) {
+        
+        // Regra 1: Verificar se o e-mail já existe
+        if (usuarioRepository.findByEmail(usuario.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Erro: Este e-mail já está em uso por outro utilizador.");
+        }
+
+        // Regra 2: Verificar se o CPF
+        if (usuarioRepository.findByCpf(usuario.getCpf()).isPresent()) {
+            throw new IllegalArgumentException("Erro: Este CPF já se encontra registado no sistema.");
+        }
+
+        // Regra 3: Criptografa a palavra-passe antes de guardar
+        // O BCrypt transforma "admin123" string ilegível
+        String palavraPasseEncriptada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(palavraPasseEncriptada);
+
+        // Se passar em todas as regras, guarda o utilizador
+        return usuarioRepository.save(usuario);
+    }
+    
+    // Método auxiliar para buscar um utilizador por ID
+    public Optional<Usuario> buscarPorId(Integer id) {
+        return usuarioRepository.findById(id);
+    }
 }

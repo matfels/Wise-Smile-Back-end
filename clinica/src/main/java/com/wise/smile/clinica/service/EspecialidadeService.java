cf8104f487e0415a8806204f263475f4decc5b56
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-06-01 20:57:04 -0300

    feat: implementa classe de serviço EspecialidadeService
    Cria a classe EspecialidadeService no pacote services.
    
    Implementa as regras de negócio associadas ao gerenciamento de especialidades clínicas.

--- clinica/src/main/java/com/wise/smile/clinica/service/EspecialidadeService.java
+++ clinica/src/main/java/com/wise/smile/clinica/service/EspecialidadeService.java
@@ -1,5 +1,31 @@
 package com.wise.smile.clinica.service;
 
-public class EspecialidadeService {
+import com.wise.smile.clinica.entity.Especialidade;
+import com.wise.smile.clinica.repositories.EspecialidadeRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EspecialidadeService {
+	
+
+    @Autowired
+    private EspecialidadeRepositories especialidadeRepository;
+
+    public Especialidade salvar(Especialidade especialidade)  {
+        return especialidadeRepository.save(especialidade);
+    }
+
+    public List<Especialidade> listarTodas() {
+        return especialidadeRepository.findAll();
+    }
+
+    
+    public Optional<Especialidade> buscarPorId( Integer id)  {
+        return especialidadeRepository.findById(id);
+    }
+    
 }

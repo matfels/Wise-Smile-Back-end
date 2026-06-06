691e3f06fe85153290104c64c79f3e6931a98701
MatheusMelo96 <matheusferreirademelo@outlook.com>
2026-06-05 22:36:33 -0300

    feat: implementa TokenService para geração e validação de tokens JWT
    Cria a classe TokenService responsável pelo ciclo de vida dos tokens de autenticação.
    
    Implementa a lógica de geração de tokens seguros após a autenticação do usuário.
    
    Adiciona métodos para validação e extração do payload (como o e-mail do usuário) contido no token.

---
+++ clinica/src/main/java/com/wise/smile/clinica/service/TokenService.java
@@ -0,0 +1,60 @@
+package com.wise.smile.clinica.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.wise.smile.clinica.entity.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    // Definindo "senha mestra" para assinar os tokens. 
+    
+	private String secret = "12345678";
+
+    // Fabricamos o cracha quando o utilizador faz login
+   
+	public String gerarToken(Usuario usuario) {
+        try {
+            //Define o algoritmo de criptografia e a senha mestra
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            
+            return JWT.create()
+                    .withIssuer("API Wise Smile") //Quem emitiu o token
+                    .withSubject(usuario.getEmail() ) // A quem pertence o token (guardamos o email)
+                    .withExpiresAt(dataExpiracao() ) // Data de validade
+                    .sign(algoritmo); //Carimba a assinatura digital
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    
+	}
+
+    //Metodo que VALIDA o crachá quando o utilizador tenta aceder a uma rota protegida
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Wise Smile")
+                    .build()
+                    .verify(tokenJWT) // Tenta abrir o token com a senha mestra
+                    .getSubject(); // Devolve o email que estava guardado lá dentro
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+      
+        }
+    }
+
+    // Define 2 horas de validade do token após ser gerado.
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+   
+    }
+}
